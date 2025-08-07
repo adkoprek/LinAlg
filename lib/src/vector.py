22a972b6b50cabfa1628ea22daf214405e591c85
Adam Koprek <112286313+adkoprek@users.noreply.github.com>
2025-08-07 19:21:29 +0200

    Added vector functionality

--- lib/src/vector.py
+++ lib/src/vector.py
@@ -1,18 +1,47 @@
 from types import mat, vec
+from errors import ShapeMismatchedError
+from math import sqrt
 
+
+def vec_siz(a: vec) -> int:
+    return len(a)
 
 def vec_add(a: vec, b: vec) -> vec:
-    pass
+    if len(a) != len(b):
+        raise ShapeMismatchedError(f"The size of the vector a ({len(a)}) dosent match vector b ({len(b)})")
+
+    result: vec = []
+    for e_a, e_b in zip(a, b):
+        result.append(e_a + e_b)
+
+    return result
 
 def vec_scl(a: vec, s: float) -> vec:
-    pass
+    result: vec = []
+    for e in a:
+        result.append(s * e)
+    return result
 
 def vec_dot(a: vec, b: vec) -> float:
-    pass
+    if len(a) != len(b):
+        raise ShapeMismatchedError(f"The size of the vector a ({len(a)}) dosent match vector b ({len(b)})")
 
-def vec_dot(a: vec) -> float:
-    pass
+    result: vec = []
+    for e_a, e_b in zip(a, b):
+        result.append(e_a * e_b)
+
+    return result
+
+def vec_len(a: vec) -> float:
+    return sqrt(vec_dot(a, a))
 
 def vec_nor(a: vec) -> vec:
-    pass
+    length = vec_len(a)
+
+    result: vec = []
+    for e in a:
+        result.append(e / length)
+
+    return result
+
 
