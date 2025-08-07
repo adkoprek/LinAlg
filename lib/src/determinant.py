dde3cd3513c042c13f1ec50b6a267878fea1cb08
Adam Koprek <112286313+adkoprek@users.noreply.github.com>
2025-08-07 23:59:26 +0200

    Added determinant functonality

--- lib/src/determinant.py
+++ lib/src/determinant.py
@@ -1,2 +1,18 @@
+from types import mat, vec
+from errors import ShapeMismatchedError
+from matrix import mat_siz
+from qr import qr
+
 def det(a: list[list[float]]) -> float:
-    pass
+    rows, cols = mat_siz(a)
+    if rows != cols:
+        raise ShapeMismatchedError(f"The number of cols ({cols}) and the number of rows ({rows})")
+
+    _, R = qr(a)
+
+    det = 1
+
+    for i in range(rows):
+        det *= R[i]
+
+    return det
