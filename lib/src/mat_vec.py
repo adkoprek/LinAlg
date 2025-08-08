b18facea55e3d99ceea4e7b50ae428e78b512d06
Adam Koprek <112286313+adkoprek@users.noreply.github.com>
2025-08-08 17:43:08 +0200

    Added tests for mat_vec.py

--- lib/src/mat_vec.py
+++ lib/src/mat_vec.py
@@ -1,14 +1,19 @@
+from src.matrix import mat_siz, mat_row
+from src.vector import vec_dot
+from src.errors import ShapeMismatchedError
 from src.types import mat, vec
 
 
-def mat_vec_mul(a: list[list[float]], b: list[float]) -> list[flat]:
+def mat_vec_mul(a: list[list[float]], b: list[float]) -> list[float]:
+    _, rows = mat_siz(a)
+    if rows != len(b):
+        raise ShapeMismatchedError(f"The number of rows ({rows}) in a does not match the length ({len(b)}) of vector b")
+
     result: vec = []
 
-    for i, b_e in enumerate(b):
-        temp = 0
-        for row_e in a[i]:
-            temp += row_e * b_e
-        result.append(temp)
+    for i in range(rows):
+        row = mat_row(a, i)
+        result.append(vec_dot(row, b))
 
     return result
 
