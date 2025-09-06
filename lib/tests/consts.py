f1f6e2d4a3ae318c5b63b37a08558cd986d710a9
Adam Koprek <112286313+adkoprek@users.noreply.github.com>
2025-09-06 08:39:12 +0200

    Randomized the test for matrix.py

--- lib/tests/consts.py
+++ lib/tests/consts.py
@@ -1,1 +1,16 @@
+import numpy as np
+from random import randint
+
+
 DATA_PATH = "./lib/data"
+TEST_CASES = 50
+
+
+def random_matrix(shape: tuple[int, int] = None):
+    if shape == None:
+        rows = randint(1, 10)
+        cols = randint(1, 10)
+        return np.random.random((rows, cols))
+
+    return np.random.random(shape)
+
