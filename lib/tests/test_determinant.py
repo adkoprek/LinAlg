36b18a51be27fb751c3e36898e917a64ea70d057
Adam Koprek <112286313+adkoprek@users.noreply.github.com>
2025-09-13 19:19:47 +0200

    Randomized determinant test

--- lib/tests/test_determinant.py
+++ lib/tests/test_determinant.py
@@ -2,7 +2,7 @@ from src.determinant import det
 from src.errors import ShapeMismatchedError
 from src.types import mat, vec
 from dataclasses import dataclass, field
-from tests.consts import DATA_PATH
+from tests.consts import *
 import numpy as np
 import pytest
 import json
@@ -13,24 +13,15 @@ class DeterminantTestCase:
     a: mat
     result: float | str 
 
-data = None
-def load_data():
-    global data
-    if data is not None:
-        return
-
-    with open(f"{DATA_PATH}/determinant.json", 'r') as file:
-        data = json.load(file)
-
 def load_determinant():
-    load_data()
-    return [DeterminantTestCase(tc["a"], tc["result"]) for tc in data["determinant"]]
+    cases = []
+    for _ in range(TEST_CASES):
+        A = random_matrix(square=True)
+        cases.append(DeterminantTestCase(A, np.linalg.det(A)))
+
+    return cases
 
 @pytest.mark.parametrize("test_case", load_determinant())
 def test_determinant(test_case: DeterminantTestCase):
-    if isinstance(test_case.result, str):
-        with pytest.raises(ShapeMismatchedError):
-            det(test_case.a)
-    else:
-        result = det(test_case.a)
-        assert abs(result - test_case.result) < 1e-9
+    result = det(test_case.a)
+    assert abs(result - test_case.result) < ZERO
